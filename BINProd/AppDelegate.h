d79de108e6d80abc5b1d7269093450d9b5fc07a1
Aravind Soundararajan <aravindsoundararajan@Aravinds-MacBook-Pro.local>
2015-07-27 13:05:23 +0530

    Initial Commit

---
+++ BINProd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BINProd
+//
+//  Created by Aravind Soundararajan on 27/07/15.
+//  Copyright (c) 2015 Aravind Soundararajan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
